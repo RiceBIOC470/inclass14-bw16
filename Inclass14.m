a681a699ec6be456e90081b28ee7434b3abdf291
gbri140 <glb3@rice.edu>
2017-11-28 13:16:32 -0600

    Update Inclass14.m

--- Inclass14.m
+++ Inclass14.m
@@ -1,4 +1,9 @@
 %Inclass 14
+%GB comments
+100 Could run a loop on the threshold parameters and define the “best” mask
+2a 100 
+2b 80 No comment on which  method is better and why.  
+Overall 93
 
 %Work with the image stemcells_dapi.tif in this folder
 
